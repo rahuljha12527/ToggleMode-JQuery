dafe743252067a67513afaf4dd6cf8a59f867620
rahuljha661 <rahuljhamansapur@gmail.com>
2020-02-25 14:32:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var toggled=false;
+var circle=$("#circle");
+
+
+$("#toggle").click(function (){
+
+    $("h1").toggleClass("color-white");
+    $("body").toggleClass("bck-color-white");
+
+
+    if(!toggled){
+        circle.css("margin-left","100px");
+        toggled=true;
+    }
+    else{
+        circle.css("margin-left","1px");
+        toggled=false;
+    }
+});
